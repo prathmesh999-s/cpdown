8d9b08486870f882edb9e45f9c6410857f74f757
prathmesh999-s <professionalknowledge122@gmail.com>
2025-06-22 14:14:08 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
+api_id = "23159825"
+api_hash = "7a381846fd2b3767c0f7b4f2a52e0f33"
+bot_token = ""
 auth_users = [1713924419]
 sudo_user = 1713924419
 log_channel = -1002143629860
